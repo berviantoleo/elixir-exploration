d4ec92b857b85fb06b5c41ea46c74c6a7fb68ccd
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-09-01 08:58:46 +0700

    Fix format

--- mix.exs
+++ mix.exs
@@ -59,7 +59,7 @@ defmodule ElixirExploration.MixProject do
       {:absinthe_phoenix, "~> 2.0"},
       {:dataloader, "~> 1.0.0"},
       {:excoveralls, "~> 0.10", only: :test},
-      {:cors_plug, "~> 2.0"},
+      {:cors_plug, "~> 2.0"}
     ]
   end
 
