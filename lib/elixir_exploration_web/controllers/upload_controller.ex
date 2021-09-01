6200ff572c2e9086dbd2e103bfd711a4684d770a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-09-01 09:41:19 +0700

    add validate extension

--- lib/elixir_exploration_web/controllers/upload_controller.ex
+++ lib/elixir_exploration_web/controllers/upload_controller.ex
@@ -1,29 +1,44 @@
 defmodule ElixirExplorationWeb.UploadController do
   use ElixirExplorationWeb, :controller
+  @extension_whitelist ~w(.jpg .jpeg .gif .png .pdf)
 
   def create(conn, %{"file" => %Plug.Upload{} = upload}) do
     IO.inspect(upload)
-    path_type = Path.type(upload.filename)
 
-    if path_type !== :relative do
-      # error message
-      send_filename_error(conn)
-    else
-      # double check
-      cond do
-        upload.filename =~ ".." ->
-          send_filename_error(conn)
+    if validate(upload) do
+      path_type = Path.type(upload.filename)
 
-        upload.filename =~ "~" ->
-          send_filename_error(conn)
+      if path_type !== :relative do
+        # error message
+        send_filename_error(conn)
+      else
+        # double check
+        cond do
+          upload.filename =~ ".." ->
+            send_filename_error(conn)
 
-        true ->
-          File.mkdir("uploads")
-          file_destination = get_filename(upload.filename)
-          File.cp(upload.path, file_destination)
-          json(conn, %{status: 200, message: "OK", path: file_destination})
+          upload.filename =~ "~" ->
+            send_filename_error(conn)
+
+          true ->
+            File.mkdir("uploads")
+            file_destination = get_filename(upload.filename)
+            File.cp(upload.path, file_destination)
+            json(conn, %{status: 200, message: "OK", path: file_destination})
+        end
       end
+    else
+      send_filename_error(conn)
     end
+  end
+
+  defp validate(file) do
+    file_extension =
+      file.filename
+      |> Path.extname()
+      |> String.downcase()
+
+    Enum.member?(@extension_whitelist, file_extension)
   end
 
   defp get_filename(filename) do
@@ -37,7 +52,7 @@ defmodule ElixirExplorationWeb.UploadController do
 
   defp send_filename_error(conn) do
     conn
-    |> put_status(404)
-    |> json(%{message: "Wrong Filename"})
+    |> put_status(400)
+    |> json(%{message: "File not valid"})
   end
 end
