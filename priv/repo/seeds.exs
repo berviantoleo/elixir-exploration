b2b6c295a72d2a313087b7e348b2b81048b1399c
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-09-02 07:42:31 +0700

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,45 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+ElixirExploration.Repo.insert!(%ElixirExploration.Accounts.User{
+  email: "bervianto.leo@test.com",
+  password_hash: Argon2.hash_pwd_salt("randompwd123"),
+  role: "user",
+  name: "Bervianto"
+})
+
+ElixirExploration.Repo.insert!(%ElixirExploration.Accounts.User{
+  email: "bervianto.leo.leo@test.com",
+  password_hash: Argon2.hash_pwd_salt("randompwd123"),
+  role: "user",
+  name: "Bervianto Leo Leo"
+})
+
+ElixirExploration.Repo.insert!(%ElixirExploration.Accounts.User{
+  email: "bervianto@test.com",
+  password_hash: Argon2.hash_pwd_salt("randompwd123"),
+  role: "user",
+  name: "Bervianto Test"
+})
+
+ElixirExploration.Repo.insert!(%ElixirExploration.Accounts.User{
+  email: "bervian@test.com",
+  password_hash: Argon2.hash_pwd_salt("randompwd123"),
+  role: "user",
+  name: "Bervian"
+})
+
+ElixirExploration.Repo.insert!(%ElixirExploration.Accounts.User{
+  email: "leo@test.com",
+  password_hash: Argon2.hash_pwd_salt("randompwd123"),
+  role: "user",
+  name: "Tester"
+})
+
+ElixirExploration.Repo.insert!(%ElixirExploration.Accounts.User{
+  email: "berv@test.com",
+  password_hash: Argon2.hash_pwd_salt("randompwd123"),
+  role: "user",
+  name: "Berv"
+})
