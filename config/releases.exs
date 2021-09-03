2eaf83d516eac6ada91abb36b46536113ac5dfa7
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-09-03 17:05:51 +0700

    Add db migrate and release config

--- config/releases.exs
+++ config/releases.exs
@@ -30,12 +30,12 @@ config :elixir_exploration, ElixirExplorationWeb.Endpoint,
   ],
   secret_key_base: secret_key_base
 
-# ## Using releases (Elixir v1.9+)
+## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :elixir_exploration, ElixirExplorationWeb.Endpoint, server: true
+config :elixir_exploration, ElixirExplorationWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
