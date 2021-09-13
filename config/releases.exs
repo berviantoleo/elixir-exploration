6b1e8d5195e76212fc95047a7490032916a31fc7
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-09-13 17:25:04 +0700

    update config

--- config/releases.exs
+++ config/releases.exs
@@ -28,6 +28,7 @@ config :elixir_exploration, ElixirExplorationWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
+  url: [host: if System.get_env("APP_NAME"), do: System.get_env("APP_NAME") <> 'gigalixirapp.com', else: System.get_env("HOSTNAME"), port: 443]
   secret_key_base: secret_key_base
 
 ## Using releases (Elixir v1.9+)
