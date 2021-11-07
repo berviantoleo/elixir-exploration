943a99d7d2f74de3228a5bbadae5dbdfe00614a2
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-11-07 23:35:32 +0700

    fix test temporary

--- test/elixir_exploration_web/controllers/user_auth_test.exs
+++ test/elixir_exploration_web/controllers/user_auth_test.exs
@@ -140,23 +140,23 @@ defmodule ElixirExplorationWeb.UserAuthTest do
 
     test "stores the path to redirect to on GET", %{conn: conn} do
       halted_conn =
-        %{conn | request_path: "/foo", query_string: ""}
+        %{conn | request_path: "/", query_string: ""}
         |> fetch_flash()
         |> UserAuth.require_authenticated_user([])
 
       assert halted_conn.halted
-      assert get_session(halted_conn, :user_return_to) == "/foo"
+      assert get_session(halted_conn, :user_return_to) == "/"
 
       halted_conn =
-        %{conn | request_path: "/foo", query_string: "bar=baz"}
+        %{conn | request_path: "/", query_string: "bar=baz"}
         |> fetch_flash()
         |> UserAuth.require_authenticated_user([])
 
       assert halted_conn.halted
-      assert get_session(halted_conn, :user_return_to) == "/foo?bar=baz"
+      assert get_session(halted_conn, :user_return_to) == "/?bar=baz"
 
       halted_conn =
-        %{conn | request_path: "/foo?bar", method: "POST"}
+        %{conn | request_path: "/?bar", method: "POST"}
         |> fetch_flash()
         |> UserAuth.require_authenticated_user([])
 
